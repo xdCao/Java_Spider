f05f90948c3b67410fabe45e0eafbd1d672c5be2
caohao <705083979@qq.com>
2017-07-09 23:20:58 +0800

    这认证应该是过不去了，动态加密的

--- src/main/java/xdCao_spider_JiaoWuChu/Login.java
+++ src/main/java/xdCao_spider_JiaoWuChu/Login.java
@@ -9,6 +9,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.cookie.CookieSpecProvider;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -32,46 +33,44 @@ public class Login {
     static CookieStore cookieStore=null;
     static HttpClientContext context=null;
 
-    private static CloseableHttpClient httpClient= HttpClients.createDefault();
+    private static CloseableHttpClient httpClient= HttpClients.custom().setDefaultCookieStore(cookieStore).build();
 
     public void login() throws IOException {
 
-        String loginUrl="http://ids.xidian.edu.cn/authserver/login?service=http%3A%2F%2Fyjsxt.xidian.edu.cn%2Flogin.jsp";
-        String loginUrl2="http://jwxt.xidian.edu.cn/caslogin.jsp";
-        String dataUrl="http://yjsxt.xidian.edu.cn/student/index.jsp";
+        String loginUrl1="http://ids.xidian.edu.cn/authserver/login?service=http%3A%2F%2Fyjsxt.xidian.edu.cn%2Flogin.jsp";
 
-        HttpPost httpPost=new HttpPost(loginUrl);
+
+        HttpPost httpPost1=new HttpPost(loginUrl1);
 
         List<NameValuePair> data=new ArrayList<NameValuePair>();
 
         data.add(new BasicNameValuePair("username","1601120078"));
         data.add(new BasicNameValuePair("password","208037"));
-        data.add(new BasicNameValuePair("submit",""));
-        data.add(new BasicNameValuePair("lt","LT-31860-Nr4DeRHVPUcMaNqcVrB5XxzLecI0ev1481623540759-wHmD-cas"));
-        data.add(new BasicNameValuePair("execution","e1s1"));
-        data.add(new BasicNameValuePair("_eventId","submit"));
-        data.add(new BasicNameValuePair("rmShown","1"));
 
 
-        httpPost.setEntity(new UrlEncodedFormEntity(data));
-
-        CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
-//        System.out.println(EntityUtils.toString(httpResponse.getEntity()));
-        printResponse(httpResponse);
-        String newUrl=httpResponse.getFirstHeader("Location").getValue();
-
-        setCookieStore(httpResponse);
-        setContext();
-
-
-        System.out.println(newUrl);
-
-        HttpPost httpPost1=new HttpPost(newUrl);
-        httpPost1.setEntity(new UrlEncodedFormEntity(data));
         CloseableHttpResponse httpResponse1 = httpClient.execute(httpPost1);
         printResponse(httpResponse1);
+//        String newUrl=httpResponse1.getFirstHeader("Location").getValue();
+
+        setCookieStoreFirstTime(httpResponse1);
+
+
+//        HttpPost httpPost2=new HttpPost(dataUrl);
+//        httpPost2.setEntity(new UrlEncodedFormEntity(data));
+//        CloseableHttpResponse httpResponse2 = httpClient.execute(httpPost2);
+//        printResponse(httpResponse2);
+
+//        setCookieStoreSecondTime(httpResponse2);
+
+        String url1="http://ids.xidian.edu.cn/authserver/login;jsessionid=w6EnuYfr0-iqgEyzfV0zxch8Ml4xHq7sBAy0GjnfyWTx5OZvs-74!-258421689?service=http%3A%2F%2Fyjsxt.xidian.edu.cn%2Flogin.jsp";
+        HttpPost httpPost=new HttpPost(url1);
+        httpPost.setEntity(new UrlEncodedFormEntity(data));
+        CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+        printResponse(httpResponse);
+
 
     }
+
 
     public static void printResponse(HttpResponse httpResponse)
             throws ParseException, IOException {
@@ -84,7 +83,7 @@ public class Login {
         while (iterator.hasNext()) {
             System.out.println("\t" + iterator.next());
         }
-        // 判断响应实体是否为空
+         //判断响应实体是否为空
         if (entity != null) {
             String responseString = EntityUtils.toString(entity);
             System.out.println("response length:" + responseString.length());
@@ -93,27 +92,101 @@ public class Login {
         }
     }
 
-    public static void setCookieStore(HttpResponse httpResponse) {
+    public static void setCookieStoreFirstTime(HttpResponse httpResponse) {
         System.out.println("----setCookieStore");
         cookieStore = new BasicCookieStore();
+
+        Header[] setCookies = httpResponse.getHeaders("Set-Cookie");
+
+        //ROUTE
+        String route=setCookies[0].getValue().substring("route=".length(),setCookies[0].getValue().length());
+        System.out.println("route cookie: "+route);
+
+
         // JSESSIONID
-        String setCookie = httpResponse.getLastHeader("Set-Cookie")
-                .getValue();
-        String JSESSIONID = setCookie.substring("JSESSIONID=".length(),
-                setCookie.indexOf(";"));
-        System.out.println("JSESSIONID:" + JSESSIONID);
+        String JSESSIONID = setCookies[1].getValue().substring("JSESSIONID=".length(),
+                setCookies[1].getValue().indexOf(";"));
+        System.out.println("JSESSIONID cookie:" + JSESSIONID);
+
+        //BIGipServeridsnew.xidian.edu.cn
+        String big=setCookies[2].getValue().substring("BIGipServeridsnew.xidian.edu.cn=".length(),setCookies[2].getValue().indexOf(";"));
+        System.out.println("BIGipServeridsnew.xidian.edu.cn cookie: "+big);
+
         // 新建一个Cookie
-        BasicClientCookie cookie = new BasicClientCookie("JSESSIONID",
+        BasicClientCookie cookie0 = new BasicClientCookie("route",
+                route);
+        cookie0.setVersion(0);
+        cookie0.setDomain("127.0.0.1");
+        cookie0.setPath("/CwlProClient");
+        BasicClientCookie cookie1 = new BasicClientCookie("JSESSIONID",
                 JSESSIONID);
-        cookie.setVersion(0);
-        cookie.setDomain("127.0.0.1");
-        cookie.setPath("/CwlProClient");
+        cookie1.setVersion(0);
+        cookie1.setDomain("127.0.0.1");
+        cookie1.setPath("/CwlProClient");
+
+        BasicClientCookie cookie2 = new BasicClientCookie("BIGipServeridsnew.xidian.edu.cn",
+                big);
+        cookie2.setVersion(0);
+        cookie2.setDomain("127.0.0.1");
+        cookie2.setPath("/CwlProClient");
         // cookie.setAttribute(ClientCookie.VERSION_ATTR, "0");
         // cookie.setAttribute(ClientCookie.DOMAIN_ATTR, "127.0.0.1");
         // cookie.setAttribute(ClientCookie.PORT_ATTR, "8080");
         // cookie.setAttribute(ClientCookie.PATH_ATTR, "/CwlProWeb");
-        cookieStore.addCookie(cookie);
+        cookieStore.addCookie(cookie0);
+        cookieStore.addCookie(cookie1);
+        cookieStore.addCookie(cookie2);
+
+        List<Cookie> cookies = cookieStore.getCookies();
+        for (int i = 0; i < cookies.size(); i++) {
+            System.out.println("Local cookie: " + cookies.get(i));
+        }
+
     }
+
+
+    private void setCookieStoreSecondTime(CloseableHttpResponse httpResponse) {
+
+        System.out.println("----setCookieStore");
+
+        Header[] setCookies = httpResponse.getHeaders("Set-Cookie");
+
+        //JSESSIONID
+        String route=setCookies[0].getValue().substring("JSESSIONID=".length(),setCookies[0].getValue().length());
+        System.out.println("JSESSIONID cookie: "+route);
+
+
+        //BIGipServeridsnew.xidian.edu.cn
+        String big=setCookies[1].getValue().substring("BIGipServeridsnew.xidian.edu.cn=".length(),setCookies[1].getValue().indexOf(";"));
+        System.out.println("BIGipServeridsnew.xidian.edu.cn cookie: "+big);
+
+        // 新建一个Cookie
+        BasicClientCookie cookie0 = new BasicClientCookie("JSESSIONID",
+                route);
+        cookie0.setVersion(0);
+        cookie0.setDomain("127.0.0.1");
+        cookie0.setPath("/CwlProClient");
+
+        BasicClientCookie cookie1 = new BasicClientCookie("BIGipServeridsnew.xidian.edu.cn",
+                big);
+        cookie1.setVersion(0);
+        cookie1.setDomain("127.0.0.1");
+        cookie1.setPath("/CwlProClient");
+        // cookie.setAttribute(ClientCookie.VERSION_ATTR, "0");
+        // cookie.setAttribute(ClientCookie.DOMAIN_ATTR, "127.0.0.1");
+        // cookie.setAttribute(ClientCookie.PORT_ATTR, "8080");
+        // cookie.setAttribute(ClientCookie.PATH_ATTR, "/CwlProWeb");
+        cookieStore.addCookie(cookie0);
+        cookieStore.addCookie(cookie1);
+
+
+        List<Cookie> cookies = cookieStore.getCookies();
+        for (int i = 0; i < cookies.size(); i++) {
+            System.out.println("Local cookie: " + cookies.get(i));
+        }
+
+    }
+
 
 
     public static void setContext() {
